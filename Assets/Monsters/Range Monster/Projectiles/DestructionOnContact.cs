98581a4411816c7c9e932353420a3aadac520efa
maxime leroi <maxime.leroi@epita.fr>
2019-03-09 17:18:00 +0100

    Ranged Monster implémenté à fond
    Il manque: Tout ce qui est autours du j2 (l'archer)
    -Son script
    -adapter les mobs à deux joueurs (trouver le plus proche)
    -Les points de vie
    Et on est bon pour la soutenance 1
    Manque bien sur le multi

---
+++ Assets/Monsters/Range Monster/Projectiles/DestructionOnContact.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionOnContact : MonoBehaviour
+{
+
+    GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = this.gameObject;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(projectile);
+    }
+}
---
+++ Assets/Monsters/Range Monster/Projectiles/ProjectileMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+
+    Rigidbody projectile;
+    public float ProjectileSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // on le fait bouger tout droit
+        projectile.AddForce(transform.forward * ProjectileSpeed);
+    }
+}
