0c4f6e3052291ced1dc8e87d0c5ca2d8583ea52e
guillaume06650 <guillaume.gillet@epita.fr>
2019-04-11 18:19:58 +0200

    nique sa mere

---
+++ Assets/A implementer/HUD/Shildbash_slider/colorChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colorChange : MonoBehaviour
+{
+    Slider slider;
+    GameObject fillSlider;
+    GameObject fillOutside;
+    Image fillImageSlider;
+    Image fillImageOutside;
+    public Color ReadyColorSlider;
+    public Color ChargingColorSlider;
+    public Color ReadyColorOutside;
+    public Color CharginngColorOutside;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        fillSlider = slider.transform.GetChild(1).GetChild(0).gameObject; //grab the transform of 2nd child of slider("Fill area"), grab 1st child of that transform(Fill)
+        fillOutside = slider.transform.GetChild(0).gameObject; //grab outside of slider
+        fillImageSlider = fillSlider.GetComponent<Image>();//grab his image compenent
+        fillImageOutside = fillOutside.GetComponent<Image>();//grab image compenent
+    }
+
+    private void Update()
+    {
+        if (slider.value >= 1)
+        {
+            fillImageSlider.color = ReadyColorSlider; //slider color when full
+            fillImageOutside.color = ReadyColorOutside;//outside color when full
+        }
+        else
+        {
+            fillImageSlider.color = ChargingColorSlider; //slider color when not full
+            fillImageOutside.color = CharginngColorOutside; //outside color when not full
+        }
+
+    }
+}
---
+++ Assets/A implementer/Players/Knight/ShildBash/WindEffect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffect : MonoBehaviour
+{
+    private List<Rigidbody> EnnemyInWindAreaList = new List<Rigidbody>();
+    public Vector3 windDirection;
+    public float windStrength;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //obtention du rigidbody de l'objet entrant dans la zone 
+        if (other.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            Rigidbody objectRigid = other.gameObject.GetComponent<Rigidbody>();
+            //si il s'agit d'un ennemy on l'ajoute à la liste 
+            if (objectRigid.tag == "ennemy")
+                EnnemyInWindAreaList.Add(objectRigid);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //obtention du gigidbody de l'objet sortant de la zone
+        if (other.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            Rigidbody objectRigid = other.gameObject.GetComponent<Rigidbody>();
+            //si il s'agit d'un ennemy on le retire de la liste et lui retire la force
+            if (objectRigid.tag == "ennemy")
+            {
+                EnnemyInWindAreaList.Remove(objectRigid);
+                objectRigid.velocity = new Vector3(0, 0, 0);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(EnnemyInWindAreaList.Count > 0)
+        {
+            //déplace tous les objet présent dans la zone vers l'extérieur
+            foreach(Rigidbody ennemy in EnnemyInWindAreaList)
+            {
+                ennemy.velocity = (windDirection * windStrength);
+            }
+        }
+    }
+}
