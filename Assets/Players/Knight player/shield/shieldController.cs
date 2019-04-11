c324e5b32b3c35413c17797652febe1ac316f6a0
guillaume06650 <guillaume.gillet@epita.fr>
2019-04-11 17:39:54 +0200

    g

--- Assets/Players/Knight player/shield/shieldController.cs
+++ Assets/Players/Knight player/shield/shieldController.cs
@@ -24,18 +24,14 @@ public class shieldController : MonoBehaviour
             if (MoveX == 0)
                 MoveZ = Input.GetAxisRaw("PrimaryZ");
             vect = new Vector3(MoveX, 0, MoveZ);
-            if (vect != Vector3.zero)
-                transform.rotation = Quaternion.LookRotation(vect, Vector3.up);
+            transform.rotation = Quaternion.LookRotation(vect, Vector3.up);
             transform.position = transform.parent.position;
-            transform.position += transform.forward; 
+            transform.position += transform.forward;
         }
         else
         {
-            //reposition the shield's rotation based on the parent's rotation
             transform.rotation = transform.parent.rotation;
-            //reposition the shield's position based on the parent's position
             transform.position = transform.parent.position;
-            //add a little offset to the shield position to not be fused with the parent's position
             transform.position += transform.forward;
         }
     }
