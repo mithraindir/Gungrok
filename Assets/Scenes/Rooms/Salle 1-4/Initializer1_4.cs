3b330997e6f60639204ce868137bc8efd8d3d479
maxime leroi <maxime.leroi@epita.fr>
2019-04-19 09:45:14 +0200

    4e salle
    4e salle: 3 Wizzard

---
+++ Assets/Scenes/Rooms/Salle 1-4/Initializer1_4.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Initializer1_4 : MonoBehaviourPun
+{
+    static bool HasSpawned = false;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!HasSpawned)
+        {
+
+            if (collision.gameObject.tag == "ArcherPlayer")
+            {
+                MobSpawner.Spawn("Wizzard", 385, 0, 17);
+                MobSpawner.Spawn("Wizzard", 397, 0, 4);
+                MobSpawner.Spawn("Wizzard", 385, 0, -10);
+                HasSpawned = true;
+            }
+
+        }
+    }
+}
