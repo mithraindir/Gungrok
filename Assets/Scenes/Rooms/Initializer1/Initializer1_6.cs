6fc45166d12e08dab6612e605dcc3e0694f8cf5f
maxime leroi <maxime.leroi@epita.fr>
2019-04-23 14:30:31 +0200

    Ajout de mass textures (le niveau est dégeu mais ca va changer)

---
+++ Assets/Scenes/Rooms/Initializer1/Initializer1_6.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Initializer1_6 : MonoBehaviourPun
+{
+    static bool HasSpawned = false;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (!HasSpawned)
+        {
+
+            if (collision.gameObject.tag == "ArcherPlayer")
+            {
+
+                MobSpawner.Spawn("Wizzard", 505, 0, -8);
+                MobSpawner.Spawn("Wizzard", 512, 0, 11);
+                MobSpawner.Spawn("Wizzard", 505, 0, 18);
+                MobSpawner.Spawn("Wizzard", 512, 0, -2);
+                HasSpawned = true;
+            }
+
+        }
+    }
+}
