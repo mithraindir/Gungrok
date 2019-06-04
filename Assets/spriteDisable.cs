33b4e4c3bb0940f11cdc9e970f93caedb71aff95
marvin libam <marvin.libam@epita.fr>
2019-06-04 15:36:24 +0200

    tuto element

---
+++ Assets/spriteDisable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class spriteDisable : MonoBehaviourPun
+{
+    public SpriteRenderer forKnight;
+    public SpriteRenderer forArcher;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PhotonNetwork.LocalPlayer.NickName == "Player 1")
+            forKnight.enabled = false;
+        else
+            forArcher.enabled = false;
+    }
+}
