aa0ad89b7abf099ba3e66f27cf31fb537c93a111
maxime leroi <maxime.leroi@epita.fr>
2019-06-07 00:38:39 +0200

    LA FIN

---
+++ Assets/LaFIN.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class LaFIN : MonoBehaviourPun
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == GameObject.FindGameObjectWithTag("KnightPlayer") || collision.gameObject == GameObject.FindGameObjectWithTag("ArcherPlayer"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
